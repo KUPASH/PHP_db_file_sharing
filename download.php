b95c66128107cce4b457b74f96791eceeae16ab5
KUPASH <alexshapuk@yandex.ru>
2019-07-03 22:17:45 +0300

    fifth commit

--- download.php
+++ download.php
@@ -15,19 +15,22 @@ if(isset($_SESSION['id']) && isset($_SESSION['login'])) {
         );
         $sql = 'SELECT * FROM files WHERE user_id="' . $_SESSION['id'] . '"AND image_key="' . $name . '"';
         $file = mysqli_fetch_assoc(mysqli_query($conn, $sql));
-        $realName = $file['real_name'];
-        $fileway = './uploads/' . $file['hash_name'][0] . '/' . $file['hash_name'][1] . '/' . $file['hash_name'];
+        if(!empty($file)) {
+            $realName = $file['real_name'];
+            $fileway = './uploads/' . $file['hash_name'][0] . '/' . $file['hash_name'][1] . '/' . $file['hash_name'];
 
-        if (file_exists($fileway)) {
-            header('Content-Description: File Transfer');
-            header('Content-Type: octet-stream');
-            header('Content-Disposition: attachment; filename="' . $realName . '"');
-            header('Content-Transfer-Encoding: binary');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($fileway));
-            readfile($fileway);
+
+            if (file_exists($fileway)) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: octet-stream');
+                header('Content-Disposition: attachment; filename="' . $realName . '"');
+                header('Content-Transfer-Encoding: binary');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($fileway));
+                readfile($fileway);
+            }
         }
     }
 }
